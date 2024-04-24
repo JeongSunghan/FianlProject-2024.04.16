b141ec167ff4a06d5989f2754872f19d7aa78a81
JeongSunghan <jsh000311@gmail.com>
2024-04-24 17:28:18 +0900

    프론트 수정 및 백엔드 작업 시작

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -6,7 +6,7 @@ export default function NotFound() {
   return (
     <div style={{ height: 'calc(100vh - 40px)'}}>
       <SnsBar />
-      <img src="/img/NotFound.png" alt="error" style={{ width: '100vw', height: '89vh' }} />
+      <img src="/img/NotFound.png" alt="error" style={{ width: '100vw', height: '90vh' }} />
     </div>
   )
 
